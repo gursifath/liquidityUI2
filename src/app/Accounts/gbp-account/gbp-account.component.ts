56cd22f529dbc079b8d92838816c5f735948a2b9
My name <you@gmail.com>
2019-09-11 11:22:00 +0530

    first commit

---
+++ src/app/Accounts/gbp-account/gbp-account.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gbp-account',
+  templateUrl: './gbp-account.component.html',
+  styleUrls: ['./gbp-account.component.css']
+})
+export class GbpAccountComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/Accounts/usd-account/usd-account.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-usd-account',
+  templateUrl: './usd-account.component.html',
+  styleUrls: ['./usd-account.component.css']
+})
+export class UsdAccountComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
